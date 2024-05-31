c9d19b503df3d46104f34c71f13dbbe6df95be3e
Gabriel <gmyuseff@uc.cl>
2024-05-31 17:43:00 -0400

    Added C# Project + updated Project Statement

---
+++ CSharp_project/SoccerSim/SoccerSim/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SoccerSim
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void HelloWorld_button_Click(object sender, RoutedEventArgs e)
+        {
+            switch (HelloWorld_button.Content)
+            {
+                case "Click Here":
+                    HelloWorld_button.Content = "Hello World!";
+                    break;
+                default:
+                    HelloWorld_button.Content = "Click Here";
+                    break;
+            }
+
+        }
+    }
+}
